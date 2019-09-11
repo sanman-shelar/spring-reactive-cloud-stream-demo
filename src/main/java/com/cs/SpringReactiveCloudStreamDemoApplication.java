661e47744cf8578dc17a7d70256c88b89d619d11
Sanman Shelar <39533009+sanman-shelar@users.noreply.github.com>
2019-09-11 23:39:45 +0530

    Initial Commit

---
+++ src/main/java/com/cs/SpringReactiveCloudStreamDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveCloudStreamDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveCloudStreamDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cs/TestMessage.java
@@ -0,0 +1,23 @@
+package com.cs;
+
+public class TestMessage {
+
+	private String message;
+	
+	public TestMessage() {
+		super();
+	}
+
+	public TestMessage(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}	
+}
